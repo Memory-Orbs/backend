eeabfceb29eddfd2866835c3d39aa161841e1f77
your_name <orkunkorcak@gmail.com>
2025-12-30 17:38:36 +0300

    first start

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { env } from './utils/env.js';
+
+dotenv.config();
+const PORT = Number(env('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+ 
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello, Welcome to Memory Orbs app...!',
+    });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
